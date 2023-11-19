51d585893225e9e72e4da8aff9b5edc72fe79730
Julius42D <julius.buller@42digital.de>
2023-11-19 23:51:49 +0100

    Trying to fix NokiaKokia font 5

--- RELOAD-Frontend/vite.config.js
+++ RELOAD-Frontend/vite.config.js
@@ -40,6 +40,6 @@ export default defineConfig({
   },
   // Add a rule for font files
   optimizeDeps: {
-    include: ['@fontsource/public/fonts/NokiaKokia.ttf'],
+    include: ['@fontsource/assets/fonts/NokiaKokia.ttf'],
   },
 });
