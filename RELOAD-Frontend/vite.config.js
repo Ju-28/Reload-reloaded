d943353446f82f655a539d4507b8f5e0b0e23281
Julius42D <julius.buller@42digital.de>
2023-11-19 23:36:27 +0100

    Fixing vite.config for font use in prod 3

--- RELOAD-Frontend/vite.config.js
+++ RELOAD-Frontend/vite.config.js
@@ -1,3 +1,23 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueJsx(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -18,17 +38,8 @@ export default defineConfig({
   build: {
     assetsDir: 'assets',
   },
-  css: {
-    preprocessorOptions: {
-      // If you're using Sass/SCSS, you can add any necessary configurations here.
-      // For example:
-      // sass: {
-      //   additionalData: `$color: red;`
-      // }
-    },
-  },
   // Add a rule for font files
   optimizeDeps: {
-    include: ['@fontsource/your-font-package'],
+    include: ['@fontsource/src/assets/fonts/NokiaKokia.ttf'],
   },
 });
