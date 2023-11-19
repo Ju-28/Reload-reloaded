907cc2a8bbb3a93f38bfa37a775e8db2e8785a58
Julius42D <julius.buller@42digital.de>
2023-11-19 23:23:46 +0100

    Fixing vite.config for font use in prod

--- RELOAD-Frontend/vite.config.js
+++ RELOAD-Frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +11,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    assetsDir: 'assets',
+  },
+});
