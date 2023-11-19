d568c70d1d066cba83766433e07b8573ba6e4337
Julius42D <julius.buller@42digital.de>
2023-11-19 23:33:21 +0100

    Fixing vite.config for font use in prod 2

--- RELOAD-Frontend/vite.config.js
+++ RELOAD-Frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,18 @@ export default defineConfig({
   },
   build: {
     assetsDir: 'assets',
+  },
+  css: {
+    preprocessorOptions: {
+      // If you're using Sass/SCSS, you can add any necessary configurations here.
+      // For example:
+      // sass: {
+      //   additionalData: `$color: red;`
+      // }
+    },
+  },
+  // Add a rule for font files
+  optimizeDeps: {
+    include: ['@fontsource/your-font-package'],
   },
 });
