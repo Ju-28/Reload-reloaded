174e206554e780ae4164ba35975a541c5bd15332
Julius42D <julius.buller@42digital.de>
2023-12-25 14:36:22 +0100

    Tweaked some styling

---
+++ RELOAD-Frontend/src/views/Galery.vue
@@ -0,0 +1,41 @@
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <div class="page-galery">
+    <div class="galery-header">
+      <h1 class="galery-headline">GALERY</h1>
+    </div>
+    <div class="galery-wrapper">
+      <img class="image1" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00001.jpeg"
+        alt="Image from last event">
+      <img class="image2" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00004.jpeg"
+        alt="Image from last event">
+      <img class="image3" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00007.jpeg"
+        alt="Image from last event">
+      <img class="image4" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00009.jpeg"
+        alt="Image from last event">
+      <img class="image6" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00010.jpeg"
+        alt="Image from last event">
+      <img class="image5" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00011.jpeg"
+        alt="Image from last event">
+      <img class="image7" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00013.jpeg"
+        alt="Image from last event">
+      <img class="image8" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00016.jpeg"
+        alt="Image from last event">
+      <img class="image9" src="https://reloadwavmedia.s3.eu-central-1.amazonaws.com/Images/image00022.jpeg"
+        alt="Image from last event">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  // name: 'Galery',
+  data() {
+  },
+  mounted() {
+    document.title = 'Galerie | RELOAD'
+  }
+}
+
+</script>
