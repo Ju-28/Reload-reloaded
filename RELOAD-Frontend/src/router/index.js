a7c9d962f669f3748ad12db21c7ac11b17079050
Julius42D <julius.buller@42digital.de>
2023-12-25 14:42:18 +0100

    Tweaked some styling

--- RELOAD-Frontend/src/router/index.js
+++ RELOAD-Frontend/src/router/index.js
@@ -51,12 +51,12 @@ const router = createRouter({
   routes
 });
 
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
-    next({ name: 'Login', query: { to: to.path } });
-  } else {
-    next()
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+//     next({ name: 'Login', query: { to: to.path } });
+//   } else {
+//     next()
+//   }
+// })
 
 export default router
